c5738bf0c44172b3b54ef5cae24a82be4fe78933
prydt <pranoydll@gmail.com>
2025-03-12 16:56:10 -0500

    fix: part 2 - check loop invariance and safe to hoist.

--- DG43932-PD9592-loop-opt-pass.cpp
+++ DG43932-PD9592-loop-opt-pass.cpp
@@ -4,96 +4,137 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
-
 #include "llvm/Analysis/DominanceFrontier.h"
 #include "llvm/Transforms/Utils/LoopUtils.h"
 #include "llvm/Analysis/ValueTracking.h"
 
 using namespace llvm;
 
-struct LoopPass : PassInfoMixin<LoopPass> {
-    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-        // errs() << "BEFORE TRANSFORM\n";
-        // F.dump();
+struct LoopPass : PassInfoMixin<LoopPass>
+{
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM)
+    {
         auto &LI = FAM.getResult<LoopAnalysis>(F);
         auto &DT = FAM.getResult<DominatorTreeAnalysis>(F);
         // Process all loops in depth-first order
-        for (Loop *L : LI) {
+        for (Loop *L : LI)
+        {
             processLoop(L, DT);
         }
 
-        // errs() << "AFTER TRANSFORM\n";
-        // F.dump();
         return PreservedAnalyses::all();
     }
 
 private:
-    void processLoop(Loop *L, DominatorTree &DT) {
+    void processLoop(Loop *L, DominatorTree &DT)
+    {
         // Process subloops first
-        for (Loop *SubLoop : L->getSubLoops()) {
+        for (Loop *SubLoop : L->getSubLoops())
+        {
             processLoop(SubLoop, DT);
         }
 
         BasicBlock *Preheader = L->getLoopPreheader();
-        if (!Preheader) return;
+        if (!Preheader)
+            return;
 
         SmallVector<Instruction *, 16> Invariants;
 
         // Collect candidate instructions in reverse order
-        for (BasicBlock *BB : L->blocks()) {
-            for (auto I = BB->rbegin(); I != BB->rend(); ++I) {
-                if (isLoopInvariant(&*I, L, DT)) {
+        for (BasicBlock *BB : L->blocks())
+        {
+            for (auto I = BB->rbegin(); I != BB->rend(); ++I)
+            {
+                I->dump();
+                if (isLoopInvariant(&*I, L, DT))
+                {
                     Invariants.push_back(&*I);
+                    errs() << "is invariant!!!!!!!!!!!!!!!!!!!!!!!!\n";
+                }
+                else
+                {
+                    errs() << "is not invariant\n";
                 }
             }
         }
 
         // Hoist safe instructions
-        for (Instruction *I : Invariants) {
-            if (safeToHoist(I, L, DT)) {
+        errs() << "HOIST PHASE\n";
+        for (Instruction *I : Invariants)
+        {
+            I->dump();
+            if (safeToHoist(I, L, DT))
+            {
                 I->moveBefore(Preheader->getTerminator());
+                errs() << "is safe to hoist\n";
+            }
+            else
+            {
+                errs() << "is not safe to hoist\n";
             }
         }
     }
 
-    bool isLoopInvariant(Instruction *I, Loop *L, DominatorTree &DT) {
-        // First check instruction type
+    bool isLoopInvariant(Instruction *I, Loop *L, DominatorTree &DT)
+    {
+        // Check allowed instruction types
         if (!isa<BinaryOperator>(I) &&
             !isa<SelectInst>(I) &&
             !isa<CastInst>(I) &&
             !isa<GetElementPtrInst>(I) &&
-            !I->isShift()) {
+            !I->isShift())
+        {
             return false;
         }
 
-        if (I->isTerminator() || isa<PHINode>(I)) {
+        // Disallowed instruction types
+        if (I->isTerminator() || isa<PHINode>(I))
+        {
             return false;
         }
-        
-        for (Value *Op : I->operands()) {
-            if (Instruction *OpInst = dyn_cast<Instruction>(Op)) {
-                if (L->contains(OpInst->getParent()) || !isLoopInvariant(OpInst, L, DT)) {
+
+        // Check all operands are either:
+        // 1. Constants, or
+        // 2. Computed outside the loop
+        for (Value *Op : I->operands())
+        {
+            if (isa<Constant>(Op))
+            {
+                continue;
+            }
+
+            if (Instruction *OpInst = dyn_cast<Instruction>(Op))
+            {
+                if (L->contains(OpInst->getParent()))
+                {
                     return false;
                 }
-            }
-            else if (!isa<Constant>(Op)) {
-                return false;
             }
         }
         return true;
     }
 
-    bool safeToHoist(Instruction *I, Loop *L, DominatorTree &DT) {
-        if (!isSafeToSpeculativelyExecute(I)) {
+    bool safeToHoist(Instruction *I, Loop *L, DominatorTree &DT)
+    {
+        if (!isSafeToSpeculativelyExecute(I, nullptr, &DT))
+        {
+            errs() << "not safe to spec exec\n";
             return false;
         }
 
-        SmallVector<BasicBlock *, 4> ExitBlocks;
-        L->getExitBlocks(ExitBlocks);
+        // Only need to check exit dominance for potentially excepting instructions
+        if (I->mayThrow())
+        {
+            SmallVector<BasicBlock *, 4> ExitBlocks;
+            L->getExitBlocks(ExitBlocks);
 
-        for (BasicBlock *Exit : ExitBlocks) {
-            if (!DT.dominates(I->getParent(), Exit)) {
-                return false;
+            for (BasicBlock *Exit : ExitBlocks)
+            {
+                if (!DT.dominates(I->getParent(), Exit))
+                {
+                    errs() << I->getParent() << " does not dominate " << Exit << '\n';
+                    return false;
+                }
             }
         }
         return true;
@@ -105,13 +146,17 @@ private:
 //-----------------------------------------------------------------------------
 // New PM Registration
 //-----------------------------------------------------------------------------
-llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo()
+{
     return {LLVM_PLUGIN_API_VERSION, "DG43932-PD9592-Loop-Opt-Pass", LLVM_VERSION_STRING,
-            [](PassBuilder &PB) {
+            [](PassBuilder &PB)
+            {
                 PB.registerPipelineParsingCallback(
                     [](StringRef Name, FunctionPassManager &FPM,
-                       ArrayRef<PassBuilder::PipelineElement>) {
-                        if (Name == "DG43932-PD9592-loop-opt-pass") {
+                       ArrayRef<PassBuilder::PipelineElement>)
+                    {
+                        if (Name == "DG43932-PD9592-loop-opt-pass")
+                        {
                             FPM.addPass(LoopSimplifyPass());
                             FPM.addPass(LoopPass());
                             return true;
@@ -125,6 +170,7 @@ llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
 // be able to recognize HelloWorld when added to the pass pipeline on the
 // command line, i.e. via '-passes=hello-world'
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
+llvmGetPassPluginInfo()
+{
     return getHelloWorldPluginInfo();
 }
