23505fc9bb186fccb426e6f403b9af2298aef4f7
Dhairya Gupta <dhairyag23@gmail.com>
2025-03-12 17:12:47 -0500

    update to llvm-19, remove prints

--- run-opt.sh
+++ run-opt.sh
@@ -1,7 +1,7 @@
 #/bin/sh
-INPUT=looptest1.ll
+INPUT=test3.ll
 OUTPUT=output.ll
 
 make -C ./build
 
-opt -load-pass-plugin ./build/libloop-opt-pass.so -passes="DG43932-PD9592-loop-opt-pass" -S <$INPUT -o $OUTPUT
+opt -load-pass-plugin ./build/libloop-opt-pass.dylib -passes="DG43932-PD9592-loop-opt-pass" -S <$INPUT -o $OUTPUT
