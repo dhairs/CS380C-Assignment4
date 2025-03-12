0bd8034eff3e8447dd7429cabc940255ca7bf0d1
prydt <pranoydll@gmail.com>
2025-03-12 12:40:04 -0500

    housekeeping

--- run-opt.sh
+++ run-opt.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-INPUT=test.ll
+INPUT=morebasicblocks.ll
 OUTPUT=output.txt
 
 opt -load-pass-plugin build/libloop-analysis-pass.so -passes="DG43932-PD9592-loop-analysis-pass" < $INPUT >/dev/null 2> $OUTPUT
